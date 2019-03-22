e05037261725217c3fbb555e0ad37d7db59fa3b4
Jose <jodaniel1409@gmail.com>
2019-03-22 13:50:03 -0600

    Implemented Game methods

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,6 +1,40 @@
 #include "game.h"
 
+Game *Game::instance = nullptr;
+
 Game::Game()
 {
+    playing = false;
+    players = new unordered_map<string, int>;
+}
 
+Game *Game::getInstance()
+{
+    if (instance == nullptr) instance = new Game();
+    return instance;
+}
+
+void Game::passTurn()
+{
+    playing = false;
+}
+
+bool Game::getPlaying() const
+{
+    return playing;
+}
+
+void Game::setPlaying(bool value)
+{
+    playing = value;
+}
+
+unordered_map<string, int> *Game::getPlayers() const
+{
+    return players;
+}
+
+void Game::setPlayers(unordered_map<string, int> *value)
+{
+    players = value;
 }
--- src/game/game.h
+++ src/game/game.h
@@ -1,10 +1,29 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include<unordered_map>
+
+using namespace std;
 
 class Game
 {
 public:
+    static Game *instance;
+
+    Game *getInstance();
+
+    void passTurn();
+
+    bool getPlaying() const;
+    void setPlaying(bool value);
+
+    unordered_map<string, int> *getPlayers() const;
+    void setPlayers(unordered_map<string, int> *value);
+
+private:
+    bool playing;
+    unordered_map<string, int> *players;  // Players names and points
+
     Game();
 };
 
